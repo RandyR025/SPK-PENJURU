db8036d8fc4a909a04327a854417aa671552f2cf
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2023-05-30 16:28:50 +0700

    Update hasil_data_penilaian.blade.php

--- SI-Penjuru2/resources/views/backend/admin/hasil_data_penilaian.blade.php
+++ SI-Penjuru2/resources/views/backend/admin/hasil_data_penilaian.blade.php
@@ -149,10 +149,10 @@ Penilaian
                     </div>
                 </div> -->
 
-                <div class="d-flex flex-row justify-content-between w-100 w-sm-50 w-xl-100">
+                <div class="d-flex flex-row justify-content-between w-100 w-sm-50 w-xl-100 mb-4">
                     <a href="{{ route('hasilpenilaian', [$item->id_penilaian, $item->id]) }}" class="btn btn-outline-primary w-100 me-1 btn-sm">Cek</a>
                 </div>
-                <div class="d-flex flex-row justify-content-between w-100 w-sm-50 w-xl-100">
+                <div class="d-flex flex-row justify-content-between w-100 w-sm-50 w-xl-100 mb-4">
                     <a href="{{ route('hasilpenilaiankepsek', [$item->id_penilaian, $item->id]) }}" class="btn btn-outline-primary w-100 me-1 btn-sm">Cek Kepsek</a>
                 </div>
                 <div class="d-flex flex-row justify-content-between w-100 w-sm-50 w-xl-100">
